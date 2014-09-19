927439472369ba53c95641813123eeb126f4bfc2
Alison Barrett <alison@10up.com>
2014-09-19 16:00:15 -0500

    Add unit tests for uncached functions

---
+++ WordPress/Tests/VIP/UncachedFunctionsUnitTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Alison Barrett <alison@10up.com>
+ */
+class WordPress_Tests_VIP_UncachedFunctionsUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of errors that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getErrorList() {
+		return array(
+			3  => 1,
+			4  => 1,
+			5  => 1,
+			6  => 1,
+			7  => 1,
+			8  => 1,
+			9  => 1,
+			10 => 1,
+			11 => 1,
+			12 => 1,
+			13 => 1,
+			14 => 1,
+			15 => 1,
+			16 => 1,
+			17 => 1,
+			30 => 1,
+		);
+
+	} //end getErrorList()
+
+
+} //end class
